16b1eeba9ce3965abc1fa03129f4828870b4ea4c
Matt Meyer <matt0719@gmail.com>
2014-10-31 13:33:50 -0400

    Revert "Reverting this"
    This reverts commit af78c1e89d74702701e59d4ac599829914863dbb.

--- ReadingList/Scripts/app.js
+++ ReadingList/Scripts/app.js
@@ -1,6 +1,5 @@
 ﻿var ViewModel = function () {
     var self = this;
-    var detailItem; 
     self.books = ko.observableArray();
     self.error = ko.observable();
 
@@ -39,14 +38,7 @@
     self.getBookDetail = function (item) {
         ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
             self.detail(data);
-            detailItem = item;
-            alert("DetailID is: " + self.detail().Id)
         });
-    }
-
-    self.markAsRead = function () {
-        self.detail().read = true;
-        ajaxHelper(booksUri + self.detail().Id, "PUT", self.detail())
     }
 
     self.authors = ko.observableArray();
