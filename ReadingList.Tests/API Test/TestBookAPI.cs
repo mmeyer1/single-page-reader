22d13b1d05bf1b19460f1119ae6647667f6da127
Matt Meyer <matt0719@gmail.com>
2014-12-04 15:14:37 -0500

    Adding unit tests, testing an async controller is difficult but I think I have the pattern now

--- ReadingList.Tests/API Test/TestBookAPI.cs
+++ ReadingList.Tests/API Test/TestBookAPI.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ReadingList.Controllers;
+﻿using BookService.Models;
 using Moq;
+using NUnit.Framework;
+using ReadingList.Controllers;
 using ReadingList.Models;
 using System.Threading.Tasks;
-
 namespace ReadingList.Tests.API_Test
-{
-    [TestClass]
+{   
+    [TestFixture]
     public class TestBookAPI
     {
-        [TestMethod]
+        [Test]
         public void TestIndex()
         {
             var fakeRepo = new Mock<IBookRepository>();  
@@ -18,13 +17,30 @@ namespace ReadingList.Tests.API_Test
             var controller = new BooksController();
             controller.repository = fakeRepo.Object; 
             var result = controller.GetBooks().ToString(); 
-            Assert.AreEqual("Title:Test,Author:Matt,ID:1", result);
+            Assert.That(result, Is.EqualTo("Title:Test,Author:Matt,ID:1"));
         }
-        [TestMethod]
-        public void TestGetBook()
+
+        [Test]
+        public void ShouldAskRepoForSpecificBook()
         {
             var fakeRepo = new Mock<IBookRepository>();
-       /*    tricksy to test asyncs, working on it*/ 
+            fakeRepo.Setup(x => x.Get(1)).ReturnsAsync(It.IsAny<BookDetailDTO>());
+            var controller = new BooksController();
+            controller.repository = fakeRepo.Object;
+            var result = controller.GetBook(1);
+            fakeRepo.Verify(fk => fk.Get(1));
+            Assert.That(true);
         } 
+
+        [Test]
+        public void ShouldCallPostNewBookCorrectly()
+        {
+            var fakeRepo = new Mock<IBookRepository>();
+            fakeRepo.Setup(x => x.Post(It.IsAny<Book>())); 
+        }
+        public void ShouldCallDelete()
+        {
+
+        }
     }
 }
